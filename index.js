5a86653bbbb6e6619673a9941dff4c259f6098f3
Jake Goritski <jgoritski@gmail.com>
2017-10-20 12:25:27 -0400

    enchilada

---
+++ index.js
@@ -0,0 +1,8 @@
+// index.js
+import moment from 'moment';
+
+console.log('Hello, world!');
+console.log('The time is: ' + moment().format());
+
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
